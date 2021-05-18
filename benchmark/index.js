c1fd7e66349c180df994c98de7694ba9d1a4e18d
Gryffon Bellish <owenbellish@gmail.com>
2021-05-18 14:54:19 +0000

    redo benchmarks

--- benchmark/index.js
+++ benchmark/index.js
@@ -3,34 +3,61 @@ const { readFile: rustReadFile, readFileSync: rustReadFileSync } = require('../'
 const { readFile: nodeReadFile } = require('fs/promises');
 const { readFileSync: nodeReadFileSync } = require('fs');
 const { join } = require('path');
+const { once, EventEmitter } = require('events');
 
 const filePath = join(__dirname, 'file.txt');
 const expected = Buffer.from('Hello, world!\nNode + rust is cool.\n'.repeat(64));
 
 function validate(result) {
-    return new Promise((resolve, reject) => {
-        expected.equals(result) ? resolve() : reject(new Error(`${result} does not match ${expected}`));
-    });
+    if (!expected.equals(result)) throw new Error(`${result} does not match ${expected}`);
 }
 
-const suite = new Suite()
-    .add('Node read file', async () => {
-        await validate(await nodeReadFile(filePath));
-    })
-    .add('Node read file sync', async () => {
-        await validate(nodeReadFileSync(filePath));
-    })
-    .add('Rust read file sync', async () => {
-        await validate(rustReadFileSync(filePath));
-    })
-    .add('Rust read file', async () => {
-        await validate(await rustReadFile(filePath));
-    })
-    .on('cycle', event => {
-        console.log(String(event.target));
-    })
-    .on('complete', () => {
-        console.log(`Fastest was ${suite.filter('fastest').map('name')}`);
-        process.exit();
-    })
-    .run({ async: true });
+const emitter = new EventEmitter();
+
+function syncSuite() {
+    const suite = new Suite('syncSuite');
+    suite
+        .add('Node read file sync', () => {
+            validate(nodeReadFileSync(filePath));
+        })
+        .add('Rust read file sync', () => {
+            validate(rustReadFileSync(filePath));
+        })
+        .on('cycle', event => {
+            console.log(String(event.target));
+        })
+        .on('complete', () => {
+            console.log(`Fastest was ${suite.filter('fastest').map('name')}`);
+            emitter.emit('syncComplete');
+        })
+        .run();
+}
+
+function asyncSuite() {
+    const suite = new Suite('asyncSuite');
+    suite
+        .add('Node read file', async () => {
+            validate(await nodeReadFile(filePath))
+        })
+        .add('Rust read file', async () => {
+            validate(await rustReadFile(filePath))
+        })
+        .on('cycle', event => {
+            console.log(String(event.target));
+        })
+        .on('complete', () => {
+            console.log(`Fastest was ${suite.filter('fastest').map('name')}`);
+            emitter.emit('asyncComplete');
+        })
+        .run({ async: true });
+}
+
+async function bench() {
+    const promises = Promise.all([once(emitter, 'syncComplete'), once(emitter, 'asyncComplete')]);
+    syncSuite();
+    asyncSuite();
+    await promises;
+    process.exit();
+}
+
+bench();
