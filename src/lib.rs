37c371a194fb0c721ae68da490a1705a72262558
Gryffon Bellish <owenbellish@gmail.com>
2021-05-23 03:10:40 +0000

    add another thing

--- src/lib.rs
+++ src/lib.rs
@@ -14,10 +14,11 @@ mod read_file;
 mod write_file;
 
 #[module_exports]
-fn init(mut exports: JsObject) -> Result<()> {
+fn init(mut exports: JsObject, env: Env) -> Result<()> {
     exports.create_named_method("readFileSync", read_file_sync)?;
     exports.create_named_method("readFile", read_file)?;
     exports.create_named_method("writeFileSync", write_file_sync)?;
     exports.create_named_method("writeFile", write_file)?;
+    exports.set_named_property("napiVersion", env.create_uint32(env.get_napi_version()?)?)?;
     Ok(())
 }
